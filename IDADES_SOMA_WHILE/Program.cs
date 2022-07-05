326a2618458e43b03e864dce7725ee2658f2ce10
DianaIannoniPolaquini <108705643+DianaIannoniPolaquini@users.noreply.github.com>
2022-07-05 00:08:48 -0300

    primeiro commit

---
+++ IDADES_SOMA_WHILE/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace IDADES_SOMA_WHILE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double soma = 0;
+            int idade, cont;
+            cont = 0;
+
+            do
+            {
+                Console.WriteLine("Entre com a sua idade: ");
+                idade = int.Parse(Console.ReadLine());
+                soma = soma + idade;
+                cont++;
+            }
+            while (idade > 0);
+
+            Console.WriteLine();
+            double media = (soma - idade)/--cont;
+            Console.WriteLine($"Média das idades {media.ToString("F2", CultureInfo. InvariantCulture)}.");
+        }
+    }
+}
